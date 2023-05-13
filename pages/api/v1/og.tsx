6e158542fb347fc8bfcceb99fef6013473995ede
koki-develop <kou.pg.0131@gmail.com>
2023-05-14 08:53:32 +0900

    シンプルな og 画像を生成できるようにした

---
+++ pages/api/v1/og.tsx
@@ -0,0 +1,38 @@
+import React from "react";
+import { ImageResponse } from "next/server";
+
+export const config = {
+  runtime: "edge",
+};
+
+export default async function GET(request: Request) {
+  const url = new URL(request.url);
+
+  const t = url.searchParams.get("t");
+  if (!t) return new Response("Bad Request", { status: 400 });
+
+  return new ImageResponse(
+    (
+      <div
+        style={{
+          fontSize: 40,
+          color: "black",
+          background: "white",
+          width: "100%",
+          height: "100%",
+          padding: "50px 200px",
+          textAlign: "center",
+          justifyContent: "center",
+          alignItems: "center",
+          display: "flex",
+        }}
+      >
+        {t}
+      </div>
+    ),
+    {
+      width: 1200,
+      height: 630,
+    }
+  );
+}
